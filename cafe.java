13dfd5c954e14ff56fadfdd63228323a6eae6313
Abukuseboaz <boazabukuse.o@gmail.com>
2026-01-02 12:20:15 +0300

    Refactor CoffeeOrder class and add comments
    Refactor CoffeeOrder class with comments and formatting.

--- cafe.java
+++ cafe.java
@@ -1,12 +1,13 @@
 class CoffeeOrder {
+    // initializing variables
     String size;
     double price;
-
+// constructor
     CoffeeOrder(String size, double price) {
         this.size= size;
         this.price= price;
     }
-
+//display method for coffe details
     void display(){
         System.out.println("Coffee Size: " +size);
         System.out.println("Price:Ksh " +price);
@@ -18,11 +19,12 @@ public class Cafe {
     public static void main(String[] args) {
         CoffeeOrder coffeeorder1= new CoffeeOrder("Large", 200);
         CoffeeOrder coffeeorder2= new CoffeeOrder("Small", 100);
-
+// display coffee order details
         System.out.println("Coffee order: ");
         coffeeorder1.display();
 
         System.out.println("Coffee order: ");
         coffeeorder2.display();
     }
+
 }
